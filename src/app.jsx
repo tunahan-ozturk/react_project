3f37b98ec2b693c87f64d81930d73600a4f44f4b
Tunahan Öztürk <tnhn17@windowslive.com>
2023-06-12 20:20:15 +0300

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import viteLogo from '/vite.svg'
+import './app.css'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return <div className="App">New project with React</div>;
+}
+
+export default App; 
